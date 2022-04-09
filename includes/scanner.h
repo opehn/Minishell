cae3a754d68f285f2db0f6323077d0135af159bd
acho <opehn0@gmail.com>
2022-04-10 00:01:52 +0900

    1

---
+++ includes/scanner.h
@@ -0,0 +1,17 @@
+typedef enum e_token
+{
+    CMD = 0,
+    OPT = 1,
+    FD = 2,
+    FNAME = 3,
+    LEFT_RED = 4,
+    RIGHD_RED = 5,
+    LD_RED = 6,
+    RD_RED = 7,
+    LSQ = 8, //left single quote
+    RSQ = 9, //right single quote
+    LDQ = 10, //left double quote
+    RDQ = 11, //right double quote
+	DS = 12, //dollar sign
+	backslash = 13, //backslash
+} t_token;
