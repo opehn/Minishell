9177b52adc20183f1cdc574bb8ad20af1b9c1e7f
acho <opehn0@gmail.com>
2022-05-02 16:40:33 +0900

    1

--- srcs/parser/parsing.c
+++ srcs/parser/parsing.c
@@ -6,7 +6,7 @@
 /*   By: taeheoki <taeheoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 15:05:23 by taeheoki          #+#    #+#             */
-/*   Updated: 2022/04/29 17:21:52 by taeheoki         ###   ########.fr       */
+/*   Updated: 2022/05/02 16:27:44 by acho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 #include "error.h"
 #include "parsing.h"
 #include "free_data.h"
+#include <stdio.h>
+
+char	*test_type(int	type)
+{
+	if (type == 1)
+		return ("INPUT_RED");
+	else if (type == 2)
+		return ("OUTPUT_RED");
+	else if (type == 3)
+		return ("APPEND_RED");
+	else if (type == 4)
+		return ("HEREDOC");
+	else if (type == 5)
+		return ("CMD");
+	else if (type == 6)
+		return ("OPTARG");
+	else if (type == 7)
+		return ("BRANCH");
+	return (NULL);
+}
 
 void	free_pipe_body(t_pipe_list *pipe)
 {
@@ -46,6 +66,32 @@ int	parsing(t_info *info, char *input)
 	if (!pipe->pipe_data)
 		return (0);
 	res = parsing_tree(info, pipe);
+
+int	i;
+	int	j;
+	i = 0;
+	t_tree	*temp;
+	while (i < count_tree(pipe))
+	{
+		j = 0;
+		temp = info->root[i];
+		printf("---------%d 번째 파이프 ------------\n", i);
+		while (temp->right_child)
+		{
+			if (!temp->right_child->right_child)
+			{
+				printf("%s \t\t\ttoken : %s\n", test_type(temp->left_child->type), temp->left_child->data);
+				printf("%s \t\t\ttoken : %s\n", test_type(temp->right_child->type), temp->right_child->data);
+				break;
+			}
+			else
+				printf("%s \t\ttoken : %s\n", test_type(temp->left_child->type), temp->left_child->data);
+			temp = temp->right_child;
+			j++;
+		}
+		i++;
+	}
+
 	if (res)
 	{
 		free_pipe_body(pipe);
